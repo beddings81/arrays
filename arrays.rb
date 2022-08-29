21761066b3b1a6989cac54de317edf022acdbef5
beddings81 <111210465+beddings81@users.noreply.github.com>
2022-08-29 11:57:10 -0500

    Add 2 more arrays

--- arrays.rb
+++ arrays.rb
@@ -1,2 +1,4 @@
 Family_name = ["Tiffany", "Jay", "Sydney", "Ari"]
 Family_ages = ["50", "28", "23", "1"]
+Family_dob = ["03.01.1972", "10.14.1993", "05.26.1999", "10.01.2020"]
+Over_18 = ["True", "True", "True", "False"]
