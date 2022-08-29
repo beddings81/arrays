4f96b42a3e1c8a0063cc336f639fe351c1f1645a
beddings81 <111210465+beddings81@users.noreply.github.com>
2022-08-29 13:05:40 -0500

    Add index position explanation

--- arrays.rb
+++ arrays.rb
@@ -11,10 +11,16 @@ Family_name.pop
 #`Family_ages.push("5", "13")` would add 5 & 3 to the end of array Family_ages.
 Family_ages.push("5", "13")
 
-#The method shift remove the first element of an array. If given a index position it will remove the exact element
+#The method shift remove the first element of an array. If given a index position
+#it will remove the exact element
 #`Family_dob.shift` would remove the element 03.01.1972 from the array
 Family_dob.shift
 
 #The method unshift adds to the front of an array
 #`Over_18.shift("False")` would add the element false to the front of the array
 Over_18.unshift("False")
+
+#To access one element from an array, bracket notation and a number that
+#corresponds with that element should be used. As weird as it may seem, counting
+# starts with 0 in most programming languages. That number is referred to as an
+#index position.
